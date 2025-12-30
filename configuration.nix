dfe65ca83183c051c7afde505927f891b2948857
Om <Om@Babel.local>
2025-12-30 13:05:41 +0100

    +Tailscale

--- configuration.nix
+++ configuration.nix
@@ -19,6 +19,8 @@
   networking.hostName = "BabelNix"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
+  services.tailscale.enable = true;
+
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
