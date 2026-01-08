c3d9fe604652ceb6273afec215e08ad720f7cf3e
Om <Om@babel.local>
2026-01-08 20:43:29 +0100

    + shell alias easy nav

--- modules/core/shell.nix
+++ modules/core/shell.nix
@@ -2,6 +2,7 @@
 {
   environment.shellAliases = {
     ll = "ls -al";
+    cdc = "cd /etc/nixos";
     econf = "nvim /etc/nixos"; # Edit configuration (use sudo if not root)
     rebuild = "nixos-rebuild switch --flake";
   };
