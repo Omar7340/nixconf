158269f97c99e439acb2269a8926ade40b9c0091
kage <kage@localhost.local>
2026-02-05 02:03:53 +0100

    ~ fix wsl build, needed system specified (and import module fixed)

--- modules/nixosModules/hosts/wsl/configuration.nix
+++ modules/nixosModules/hosts/wsl/configuration.nix
@@ -3,20 +3,21 @@
   flake.nixosConfigurations.wsl = inputs.nixpkgs.lib.nixosSystem {
     modules = [
       self.nixosModules.hostWsl
-      self.nixosModules.git
-      self.nixosModules.nix
-      self.nixosModules.shortcuts
+      self.nixosModules.base
+      self.nixosModules.nvf
     ];
   };
 
   flake.nixosModules.hostWsl =
     { ... }:
     {
+      imports = [ inputs.nixos-wsl.nixosModules.default ];
       # Pas de bootloader ici, c'est géré par WSL
       wsl.enable = true;
       wsl.defaultUser = "nixos";
 
       networking.hostName = "nixos-wsl";
+      nixpkgs.hostPlatform = "x86_64-linux";
 
       system.stateVersion = "25.05";
     };
