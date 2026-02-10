1a49dc9d94007f1f191ad6303bf2564665569a4d
kage <kage@localhost.local>
2026-02-10 12:06:35 +0100

    + brave

---
+++ modules/nixosModules/features/browsers/browsers.nix
@@ -0,0 +1,29 @@
+{
+  flake.nixosModules.browsers =
+    { pkgs, ... }:
+    {
+      programs.chromium = {
+        enable = true;
+        extraOpts = {
+          "BrowserSignin" = 0;
+          "SyncDisabled" = true;
+          "PasswordManagerEnabled" = false;
+          "SpellcheckEnabled" = true;
+          "SpellcheckLanguage" = [
+            "fr"
+            "en-US"
+          ];
+        };
+
+        extensions = [
+          "eimadpbcbfnmbkopoojfekhnkhdbieeh" # Dark Reader
+          "cjpalhdlnbpafiamejdnhcphjbkeiagm" # Ublock Origin
+        ];
+      };
+      environment.etc."/brave/policies/managed/GroupPolicy.json".source = ./policies.json;
+
+      environment.systemPackages = [
+        pkgs.brave
+      ];
+    };
+}
