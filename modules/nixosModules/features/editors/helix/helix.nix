6c798b955694ca5eac61c173357a40966845c36d
kage <kage@localhost.local>
2026-02-14 19:33:48 +0100

    + markdown oxide

--- modules/nixosModules/features/editors/helix/helix.nix
+++ modules/nixosModules/features/editors/helix/helix.nix
@@ -13,15 +13,14 @@
       hjem.users.${config.preferences.user.name} = {
         files = {
           ".config/helix/languages.toml".source = ./languages.toml;
-
           ".config/helix/config.toml".source = ./config.toml;
+          ".config/.moxide.toml".source = ./moxide.toml;
         };
       };
 
       hjem.users.root = {
         files = {
           ".config/helix/languages.toml".source = ./languages.toml;
-
           ".config/helix/config.toml".source = ./config.toml;
         };
       };
