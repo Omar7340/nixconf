864190f1d0758a3658c665934d5d3fc2c66a9ab8
kage <kage@localhost.local>
2026-03-06 23:48:14 +0100

    + Freecad

--- modules/nixosModules/features/multimedia.nix
+++ modules/nixosModules/features/multimedia.nix
@@ -1,11 +1,28 @@
 {
-  flake.nixosModules.multimedia =
-    { pkgs, ... }:
-    {
-      environment.systemPackages = with pkgs; [
-        qbittorrent
-        vlc
-        pear-desktop
+  flake.nixosModules.multimedia = {
+    self,
+    pkgs,
+    ...
+  }: let
+    # FIX for issue https://github.com/NixOS/nixpkgs/issues/468456
+    freecadWayland = pkgs.symlinkJoin {
+      name = "freecad-wayland-fix";
+      paths = [
+        pkgs.freecad-wayland
       ];
+      buildInputs = [pkgs.makeWrapper];
+      postBuild = ''
+        wrapProgram $out/bin/FreeCAD \
+          --prefix MESA_LOADER_DRIVER_OVERRIDE : zink \
+          --prefix __EGL_VENDOR_LIBRARY_FILENAMES : ${pkgs.mesa}/share/glvnd/egl_vendor.d/50_mesa.json \
+      '';
     };
+  in {
+    environment.systemPackages = with pkgs; [
+      qbittorrent
+      vlc
+      pear-desktop
+      freecadWayland
+    ];
+  };
 }
