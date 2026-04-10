d72f5f63a72aef35c54a55c93adfba5136e9b49f
kage <kage@localhost.local>
2026-04-10 20:36:38 +0200

    + flake update and remove fix freecad

--- modules/nixosModules/features/multimedia.nix
+++ modules/nixosModules/features/multimedia.nix
@@ -3,26 +3,12 @@
     config,
     pkgs,
     ...
-  }: let
-    # FIX for issue https://github.com/NixOS/nixpkgs/issues/468456
-    freecadWayland = pkgs.symlinkJoin {
-      name = "freecad-wayland-fix";
-      paths = [
-        pkgs.freecad-wayland
-      ];
-      buildInputs = [pkgs.makeWrapper];
-      postBuild = ''
-        wrapProgram $out/bin/FreeCAD \
-          --prefix MESA_LOADER_DRIVER_OVERRIDE : zink \
-          --prefix __EGL_VENDOR_LIBRARY_FILENAMES : ${pkgs.mesa}/share/glvnd/egl_vendor.d/50_mesa.json \
-      '';
-    };
-  in {
+  }: {
     environment.systemPackages = with pkgs; [
       qbittorrent
       vlc
       pear-desktop
-      freecadWayland
+      freecad-wayland
       orca-slicer
       (mpv.override {
         scripts = [
